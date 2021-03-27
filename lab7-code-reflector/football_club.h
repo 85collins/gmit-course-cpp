2fdcbffbb6a4250e69d5a7ec9fd433ce24e0ab95
85collins <72947295+85collins@users.noreply.github.com>
2021-03-27 17:26:19 +0000

    Add files via upload

---
+++ lab7-code-reflector/football_club.h
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+#include "player.h"
+
+class FootballClub{
+private:
+
+    string name;
+    string clubname;
+    string district;
+    string stripColour;
+
+    // const static int , way to make a constant variable in a class
+    const static int MAXSQUAD = 23;
+    //const static int MAXTEAM = 15;
+    Player squad[MAXSQUAD];
+    //Player team[MAXTEAM];
+
+
+
+public:
+
+
+    //contructor
+
+    FootballClub();
+    FootballClub(string clubname, string district);
+
+    string getClubname();
+    string getDistrict();
+    string getManagerName();
+
+    void setManagerName(string Name);
+    void printManager();
+
+    void setClubname(string clubname);
+    void setDistrictname(string district);
+    void printInfo();
+
+
+    void addPlayerToSquad(Player tmp, int position);
+    //void addPlayerToTeam(Player tempPlayer,int ID);
+
+    void printTeam();
+    void printSquad();
+};
+
---
+++ lab7-code-reflector/main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+#include "football_club.h"
+
+void test(){
+
+/*
+string galwayTeam[2][]=   {{"Ruairi", "Lavelle"},
+                            {"Eoghan", "Kerin"},
+                            {"Sean Andy", "Ó Ceallaigh"},
+                            {"David", "Wynne"},
+                            {"Gary", "O'Donnell"},
+                            {"Gareth", "Bradshaw"},
+                            {"Johnny", "Heaney"},
+                            {"Thomas", "Flynn"},
+                            {"Ciarán", "Duggan"},
+                            {"Michael", "Daly"},
+                            {"Johnathan", "Duane"},
+                            {"Eamon", "Brannigan"},
+                            {"Barry", "McHugh"},
+                            {"Padraig", "Cunningham"},
+                            {"Antaine", "Ó Laoí"}}
+
+*/
+
+}
+
+
+
+
+int main()
+{
+    FootballClub club;
+    club.printInfo();
+    club.setClubname("Castletown");
+    club.setDistrictname("Westmeath");
+    club.setManagerName("Sir Alex");
+
+    string clubName = club.getClubname();
+    clubName = club.getDistrict();
+    cout << clubName<< endl;
+
+    Player playerTemp;
+
+    //READING DATA FROM A FILE
+    fstream infile;
+    infile.open("teamSquad.csv", ios::in);
+    string number,firstName, secondName, dob, position, mobile;
+    string line;
+    int i = 0;
+    while(std::getline(infile, line)){
+        std::istringstream strLine(line);
+        if(!(strLine>>number>>firstName>>secondName>>dob>>position>>mobile)){
+            break;
+        }
+        else{
+            cout<<"Input ->"<<number<<" "<<firstName<<" "<<secondName<<" "<<dob<<" "<<position<<" "<<mobile<<endl;
+            playerTemp.setNumber(stoi(number));
+            playerTemp.setName(firstName,secondName);
+            //note stoi convert a string to a int
+            playerTemp.setDOB(stoi(dob));
+            playerTemp.setPosition(position);
+            playerTemp.setMobileNumber(mobile);
+            club.addPlayerToSquad(playerTemp, i);
+        }
+        //increments
+        i =i+1;
+    }
+    infile.close();
+
+
+    cout<<"<<---------------->>"<<endl;
+    cout<<"SQUAD"<<endl;
+    club.printSquad();
+    club.printManager();
+
+    /*
+    club.printManager* ptr = new printManager();   Tried implement all of Question 3 & 4
+    ptr->getManagerName();                         but couldnt get it to work properly.
+    delete ptr;
+    */
+
+
+
+
+    return 0;
+}
