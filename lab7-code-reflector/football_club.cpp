42b37e96b8da37ca888a63616bd9c23aff136a6a
85collins <72947295+85collins@users.noreply.github.com>
2021-03-28 15:36:38 +0100

    Add files via upload

---
+++ lab7-code-reflector/football_club.cpp
@@ -0,0 +1,74 @@
+#include"football_club.h"
+
+
+    FootballClub::FootballClub(){
+        this->clubname = "NA";
+        this->district = "NA";
+    }
+    FootballClub::FootballClub(string clubname, string district){
+        this->clubname = clubname;
+        this->district = district;
+    }
+    string FootballClub::getManager(){
+        return name;
+    }
+    string FootballClub::getClubname(){
+        return clubname;
+    }
+    string FootballClub::getDistrict(){
+        return district;
+    }
+    void FootballClub::setManager(string forename,string surname){
+        Manager->setName(forename,surname);
+    }
+    void FootballClub::setClubname(string clubname){
+        this->clubname = clubname;
+    }
+
+    void FootballClub::setDistrictname(string district){
+        this->district = district;
+    }
+
+    void FootballClub::printInfo(){
+     cout<<clubname<<endl;
+    }
+
+
+    void FootballClub::addPlayerToSquad(Player tmp, int position){
+        this->squad[position].setNumber(tmp.getNumber());
+        this->squad[position].setName(tmp.getForename(), tmp.getSurname());
+        this->squad[position].setPosition(tmp.getPosition());
+        this->squad[position].setDOB(tmp.getDOB());
+        this->squad[position].setPosition(tmp.getPosition());
+        this->squad[position].setMobileNumber(tmp.getMobileNumber());
+
+    }
+    void FootballClub::printTeam(){
+
+        for(int i=0;i<MAXSQUAD;i++){
+            squad[i].printPlayerInfo();
+        }
+
+    }
+    void FootballClub::printSquad(){
+int num;
+        for(int i=0;i<MAXSQUAD;i++){
+                if(squad[i].getNumber()<=15)
+                {
+                squad[i].printPlayerInfo();
+                cout<< " -----Starting on the Team"<<endl;
+                }
+                else
+                {
+                    squad[i].printPlayerInfo();
+                    cout<< " -----Substitute"<<endl;
+                }
+
+        }
+
+    }
+
+    void FootballClub::printManager(){
+    cout<< "Manager -"<<Manager->getForename()<<""<<Manager->getSurname()<<endl;
+    }
+
---
+++ lab7-code-reflector/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+#include "football_club.h"
+
+void test(){
+
+/*
+string galwayTeam[2][]=   {{"Ruairi", "Lavelle"},
+                            {"Eoghan", "Kerin"},
+                            {"Sean Andy", "Ó Ceallaigh"},
+                            {"David", "Wynne"},
+                            {"Gary", "O'Donnell"},
+                            {"Gareth", "Bradshaw"},
+                            {"Johnny", "Heaney"},
+                            {"Thomas", "Flynn"},
+                            {"Ciarán", "Duggan"},
+                            {"Michael", "Daly"},
+                            {"Johnathan", "Duane"},
+                            {"Eamon", "Brannigan"},
+                            {"Barry", "McHugh"},
+                            {"Padraig", "Cunningham"},
+                            {"Antaine", "Ó Laoí"}}
+
+*/
+
+}
+
+
+
+
+int main()
+{
+    FootballClub club;
+    club.printInfo();
+    club.setClubname("Castletown");
+    club.setDistrictname("Westmeath");
+    club.setManager("Sir Alex"," Ferguson");
+
+    string clubName = club.getClubname();
+    clubName = club.getDistrict();
+    cout << clubName<< endl;
+
+    Player playerTemp;
+
+    //READING DATA FROM A FILE
+    fstream infile;
+    infile.open("teamSquad.csv", ios::in);
+    string number,firstName, secondName, dob, position, mobile;
+    string line;
+    int i = 0;
+    while(std::getline(infile, line)){
+        std::istringstream strLine(line);
+        if(!(strLine>>number>>firstName>>secondName>>dob>>position>>mobile)){
+            break;
+        }
+        else{
+            cout<<"Input ->"<<number<<" "<<firstName<<" "<<secondName<<" "<<dob<<" "<<position<<" "<<mobile<<endl;
+            playerTemp.setNumber(stoi(number));
+            playerTemp.setName(firstName,secondName);
+            //note stoi convert a string to a int
+            playerTemp.setDOB(stoi(dob));
+            playerTemp.setPosition(position);
+            playerTemp.setMobileNumber(mobile);
+            club.addPlayerToSquad(playerTemp, i);
+        }
+        //increments
+        i =i+1;
+    }
+    infile.close();
+
+
+    cout<<"<<---------------->>"<<endl;
+    cout<<"SQUAD"<<endl;
+    club.printSquad();
+    club.printManager();
+
+
+
+
+
+    return 0;
+}
